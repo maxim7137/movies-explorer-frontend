e5d432a8a74c912994db3953a135e0550f16db12
maxim7137 <maxim713@mail.ru>
2023-02-05 10:30:06 +0300

    feature: header и footer подогнал расстояния до main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import AboutProject from './AboutProject';
 import Techs from './Techs';
 import AboutMe from './AboutMe';
 import Portfolio from './Portfolio';
+// import Preloader from '../Preloader/Preloader';
 
 function Main() {
   return (
