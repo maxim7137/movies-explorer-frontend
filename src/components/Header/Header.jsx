134ec54389e1ed238e4fa9097ae16c884b26a271
maxim713 <maxim713@mail.ru>
2023-02-07 23:43:39 +0300

    feature: начать 404

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
 import Navigation from '../Navigation/Navigation';
+import Logo from './Logo';
 
 function Header({ loggedIn, logIn, logOut }) {
   return (
     <header className="header">
-      <div className="logo">
-        <div className="logo__circle">
-          <div className="logo__arc"></div>
-        </div>
-      </div>
+      <Logo />
       <Navigation loggedIn={loggedIn} logIn={logIn} logOut={logOut} />
     </header>
   );
---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,11 @@
+function Logo() {
+  return (
+    <div className="logo">
+      <div className="logo__circle">
+        <div className="logo__arc"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
