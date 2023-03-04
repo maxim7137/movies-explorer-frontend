39612b31fc5530488419cfd5edc9fa81c09a09d7
maxim713 <maxim713@mail.ru>
2023-03-04 18:53:39 +0300

    feature: отдельная старничка с информацией о фильме

--- src/components/Movies/Movie.jsx
+++ src/components/Movies/Movie.jsx
@@ -9,7 +9,6 @@ function Movie({ cardsBeatfilm }) {
     duration,
     year,
     description,
-    trailerLink,
     nameRU,
     nameEN,
     image,
@@ -17,32 +16,34 @@ function Movie({ cardsBeatfilm }) {
 
   return (
     <section className="movie content">
-      <h1 className="movie__title">{nameRU}</h1>
-      <p className="movie__subtitle">{nameEN}</p>
-      <div className="movie__poster">
-        <img className="movie__image" alt="Постер" src={image}></img>
-      </div>
-      <h2 className="movie__about">О фильме</h2>
-      <div className="movie__table">
-        <div className="movie__row">
-          <p className="movie__column movie__key">Год</p>
-          <p className="movie__column movie__value">{year}</p>
+      <div className="movie__container">
+        <h1 className="movie__title">{nameRU}</h1>
+        <p className="movie__subtitle">{nameEN}</p>
+        <div className="movie__poster">
+          <img className="movie__image" alt="Постер" src={image}></img>
         </div>
-        <div className="movie__row">
-          <p className="movie__column movie__key">Страна</p>
-          <p className="movie__column movie__value">{country}</p>
-        </div>
-        <div className="movie__row">
-          <p className="movie__column movie__key">Режиссер</p>
-          <p className="movie__column movie__value">{director}</p>
-        </div>
-        <div className="movie__row">
-          <p className="movie__column movie__key">Время</p>
-          <p className="movie__column movie__value">{duration} мин.</p>
-        </div>
-        <div className="movie__row">
-          <p className="movie__column movie__key">Описание</p>
-          <p className="movie__column movie__value">{description}</p>
+        <h2 className="movie__about">О фильме</h2>
+        <div className="movie__table">
+          <div className="movie__row">
+            <p className="movie__column movie__key">Год</p>
+            <p className="movie__column movie__value">{year}</p>
+          </div>
+          <div className="movie__row">
+            <p className="movie__column movie__key">Страна</p>
+            <p className="movie__column movie__value">{country}</p>
+          </div>
+          <div className="movie__row">
+            <p className="movie__column movie__key">Режиссер</p>
+            <p className="movie__column movie__value">{director}</p>
+          </div>
+          <div className="movie__row">
+            <p className="movie__column movie__key">Время</p>
+            <p className="movie__column movie__value">{duration} мин.</p>
+          </div>
+          <div className="movie__row">
+            <p className="movie__column movie__key">Описание</p>
+            <p className="movie__column movie__value movie__value_description">{description}</p>
+          </div>
         </div>
       </div>
     </section>
